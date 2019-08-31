d97432420ec47aef5d276370152adb29e2dc118d
Guido Bartolucci <guido@bartolucci.org>
2019-08-31 17:33:08 -0400

    Added script to print out pretty keeb

--- keyboards/thk_elite_c/keymaps/default/convert.py
+++ keyboards/thk_elite_c/keymaps/default/convert.py
@@ -23,7 +23,7 @@ for i, l in enumerate(r.findall(s)):
     keys = [re.sub(r'^\s*', '', k) for k in keys]
     keys = [k for k in keys if k]
     layers.append(keys)
-    print(i)
+    #print(i)
     #print(keys)
 
 
@@ -35,7 +35,59 @@ keyboard = {
     "notes": "",
     "layers": layers,
 }
-json.dump(keyboard, open('cc.json', 'w'))
+json.dump(keyboard, open('keymap.json', 'w'))
+
+keys = json.load(open('labels.json'))
+
+from colorama import Fore, Back, Style
+from more_itertools import chunked
+import unicodedata
+import ftfy
+from ftfy.formatting import display_center as center, monospaced_width
+
+def layer_key(i):
+    return chr(0x2460 + i)
+
+def color(s):
+    #return Fore.RED + Back.GREEN + s + Style.RESET_ALL
+    return Fore.GREEN + s + Style.RESET_ALL
+
+def label(k):
+    if k in keys:
+        return keys[k]
+    # UC(0x236A)
+    m = re.match(r'UC\(0x(\S+)\)', k)
+    if m:
+        n = int(m.group(1), 16)
+        return chr(n)
+    m = re.match(r'MO\((\d+)\)', k)
+    if m:
+        return layer_key(int(m.group(1)))
+    m = re.match(r'TT\((\d+)\)', k)
+    if m:
+        return layer_key(int(m.group(1)))
+    return ' '
+
+def info(k):
+    #print(monospaced_width(k))
+    return k
+
+for i, layer in enumerate(layers):
+    print(f'Layer {i}')
+    '┌', '┐'
+    print('┌─' + '──┬─'*11 + '──┐')
+    keys = list(chunked([color(center(info(label(k)), 3)) for k in layer], 12))
+    print('│' + '│'.join(keys[0]) + '│')
+    print('├─' + '──┼─'*11 + '──┤')
+    print('│' + '│'.join(keys[1]) + '│')
+    print('├─' + '──┼─'*11 + '──┤')
+    print('│' + '│'.join(keys[2]) + '│')
+    print('├─' + '──┼─'*11 + '──┤')
+    print('│' + '│'.join(keys[3]) + '│')
+    print('└─' + '──┴─'*11 + '──┘')
+    quit()
+
+
 
 
 #    {
@@ -43,13 +95,8 @@ json.dump(keyboard, open('cc.json', 'w'))
 #  "keymap": "planck_rev5_layout_ortho_4x12_mine",
 #  "layout": "LAYOUT_planck_1x2uC",
 #  "layers": [
-#	      ["KC_TAB",
+#         ["KC_TAB",
 #  ],
 #  "author": "Guido Bartolucci",
 #  "notes": ""
 #}
-
-
-
-
-
